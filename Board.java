76da1f12dc825920f87f53f231ec0b380d021216
Mads Grau Kristensen <madsgrau@gmail.com>
2016-11-11 16:25:16 +0100

    Event cards for board

--- Board.java
+++ Board.java
@@ -1,14 +1,51 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Board {
+    public enum EventType {OnTurnStart, OnTurnEnd, OnCardPlayed, OnCardDraw}
+
     ArrayList<FunctionCard> functionCards;
+    HashMap<EventType, FunctionCard> eventFunctions = new HashMap<>();
     
     Board() {
-        
+        for (EventType eventType : EventType.values()) {
+            eventFunctions.put(eventType, null);
+        }
     }
     
     void addCardToFuction(Card card, int functionIndex, int placeIndex) {
         
     }
-    
+
+    public void OnTurnStart(Player player, Player other) {
+        FunctionCard functionCard = eventFunctions.get(EventType.OnTurnStart);
+
+        if (functionCard != null) {
+            functionCard.execute(player, player);
+        }
+    }
+
+    public void OnTurnEnd(Player player, Player other) {
+        FunctionCard functionCard = eventFunctions.get(EventType.OnTurnEnd);
+
+        if (functionCard != null) {
+            functionCard.execute(player, player);
+        }
+    }
+
+    public void OnCardPlayed(Player player, Player other) {
+        FunctionCard functionCard = eventFunctions.get(EventType.OnCardPlayed);
+
+        if (functionCard != null) {
+            functionCard.execute(player, player);
+        }
+    }
+
+    public void OnCardDraw(Player player, Player other) {
+        FunctionCard functionCard = eventFunctions.get(EventType.OnCardDraw);
+
+        if (functionCard != null) {
+            functionCard.execute(player, player);
+        }
+    }
 }
