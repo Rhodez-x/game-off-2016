3bdf0c771496015e4390ca0a81e8b3e47cd60ddb
Jakob Kjær-Galle <jakob@miscellus.dk>
2016-11-11 17:00:00 +0100

    Drawing stuff

--- CodeOfCards.java
+++ CodeOfCards.java
@@ -11,12 +11,13 @@ public class CodeOfCards extends PApplet {
 	}
 	
 	public void setup() {
-            game = new Game();
-            game.runGame(); // Starts the program.
+//            game = new Game();
+//            game.runGame(); // Starts the program.
 	}
 	
 	public void draw() {
-		
+		Rendering.drawCard(getGraphics(), "self.DrawCard", new PVector(200, 200));
+		Rendering.drawCard(getGraphics(), "other.DrawCard", new PVector(500, 200));
 	}
 	
     public static void main(String[] args){
