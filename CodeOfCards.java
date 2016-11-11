8eb6ea05487554624e2df7d8ad50c9950c028f03
Jakob Kjær-Galle <jakob@miscellus.dk>
2016-11-11 18:29:43 +0100

    Scratch drawing

--- CodeOfCards.java
+++ CodeOfCards.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import processing.core.*;
 
 /**
@@ -5,21 +7,47 @@ import processing.core.*;
  * @author rhodez-x
  */
 public class CodeOfCards extends PApplet {
+    float cardHeight = 70;
+    float padding = 5;
+
+    ArrayList<String> fff = new ArrayList<String>();
+
 	Game game;
 	public void settings() {
 		size(800, 600);
 	}
-	
+
 	public void setup() {
-//            game = new Game();
-//            game.runGame(); // Starts the program.
+        fff.add("Self.DrawCard()");
+        fff.add("Other.DrawCard()");
+        fff.add("Self.Life++");
+        fff.add("Other.Life++");
+        fff.add("Self.Life--");
+        fff.add("Other.Life--");
+        fff.add("Self.DiscardCard()");
+        fff.add("Other.DiscardCard()");
 	}
-	
+
 	public void draw() {
-		Rendering.drawCard(getGraphics(), "self.DrawCard", new PVector(200, 200));
-		Rendering.drawCard(getGraphics(), "other.DrawCard", new PVector(500, 200));
+		// for (FunctionCard funcCard : game.board.functionCards) {
+
+            float functionHeight = fff.size() * cardHeight + 2 * padding;
+
+            pushMatrix();
+            fill(0xaaccff);
+            translate(padding, padding);
+            rect(0, 0, 300, functionHeight);
+            translate(padding, padding);
+            fill(0xffccaa);
+            for (int cardIndex = 0; cardIndex < fff.size(); ++cardIndex) {
+                String cardText = fff.get(cardIndex);
+                translate(0, cardHeight);
+                rect(0, 0, 300-padding, cardHeight);
+            }
+            popMatrix();
+        // }
 	}
-	
+
     public static void main(String[] args){
     	PApplet.main("CodeOfCards");
     }
