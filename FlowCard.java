61d4254457ceec5cbd865d63ca2183b99889317a
Mads Grau Kristensen <mads@Madss-MacBook-Pro.local>
2016-11-11 15:07:02 +0100

    Implemented flow cards

--- FlowCard.java
+++ FlowCard.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+
 public class FlowCard extends Card{
-    int cycle; // How many times the fuction can be called.
+    int cycles; // How many times the fuction can be called.
+    ArrayList<Card> cards;
+
     @Override
     public int execute(int cyclesLeft) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (Card card : cards) {
+            if (cycles < 1) {
+                break;
+            }
+
+            cycles = card.execute(cycles);
+        }
+
+        return cycles;
     }
     
 }
