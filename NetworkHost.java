bcdd157088874b6dac358d00763fa081e39f0df6
Rhodez-x <joer0357@yahoo.dk>
2016-11-12 16:25:14 +0100

    Multi thread

--- NetworkHost.java
+++ NetworkHost.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import static java.sql.DriverManager.println;
 import java.util.Scanner;
 public class NetworkHost {
     
@@ -18,3 +19,36 @@ public class NetworkHost {
         }   
     }   
 } 
+/*
+public class MyServer { 
+    public static void main(String[]args) throws IOException { 
+        ServerSocket server = new ServerSocket(2343); 
+        while (true) { 
+            Socket sock = server.accept(); 
+            new MultiServerHandler(sock).start();     
+        }   
+    }   
+    public static class MultiServerHandler extends Thread {
+        private Socket sock;     
+        public MultiServerHandler(Socket sock) { 
+        this.sock = sock;
+        } 
+        
+        public void identify() {
+            println("This is me");
+        }
+        
+        @Override
+    public void run() {
+        try {             
+            Scanner in = new Scanner(sock.getInputStream()); 
+            PrintStream out = new PrintStream(sock.getOutputStream());
+            while (true) {  
+                out.println(in.nextLine().toUpperCase());
+                identify();  
+            }         
+        } 
+        catch (IOException e) {}     
+    }
+    }
+}*/
